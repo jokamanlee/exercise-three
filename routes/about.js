3c1aaac2e8c2609dc222dd54933da671f2c14af9
jokamanlee <jl10630@nyu.edu>
2020-10-20 15:35:04 -0400

    Finished

--- routes/about.js
+++ routes/about.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-
+//works after /about/
 router.get("/", (req, res) => res.send("About This Page:Exercise Three"));
 router.get("/me", (req, res) => res.send("About Me Page:Exercise Three"));
+router.get("/json", (req, res) =>
+  res.send({ item: "cool", itemTwo: "cooler" })
+);
 
 module.exports = router;
